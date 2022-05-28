841c6e05d79b6d150021e0f805b434b2b3dacecd
kenmaro3 <miha.ken.19@gmail.com>
2022-05-28 14:28:05 +0900

    [UPD] selecte box not mui

--- src/components/postlist/PostList.tsx
+++ src/components/postlist/PostList.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './postlist.scss'
 import PostItem from "./postitem/PostItem";
 import { RootState } from "../../store";
@@ -32,23 +32,39 @@ const PostList: FC = () => {
     const { posts, status } = useSelector((state: RootState) => state.posts)
     const [value, setValue] = React.useState(0);
     const dispatch = useDispatch()
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-        if (newValue == 0) {
-            dispatch(setSort(PostSortActions.SORT_BY_TIME))
-        }
-        else if (newValue == 1) {
-            dispatch(setSort(PostSortActions.SORT_BY_COMMENTS))
-        }
-        else if (newValue == 2) {
-            dispatch(setSort(PostSortActions.SORT_BY_LIKES))
+    const [sortType, setSortType] = useState<string>("time")
+    // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    //     setValue(newValue);
+    //     if (newValue == 0) {
+    //         dispatch(setSort(PostSortActions.SORT_BY_TIME))
+    //     }
+    //     else if (newValue == 1) {
+    //         dispatch(setSort(PostSortActions.SORT_BY_COMMENTS))
+    //     }
+    //     else if (newValue == 2) {
+    //         dispatch(setSort(PostSortActions.SORT_BY_LIKES))
 
-        }
-    };
+    //     }
+    // };
+
+    const latestSelected = () => {
+        dispatch(setSort(PostSortActions.SORT_BY_TIME))
+        setSortType("time")
+    }
+
+    const mostLikedSelected = () => {
+        dispatch(setSort(PostSortActions.SORT_BY_LIKES))
+        setSortType("like")
+    }
+
+    const mostCommentedSeleted = () => {
+        dispatch(setSort(PostSortActions.SORT_BY_COMMENTS))
+        setSortType("comment")
+    }
 
     return (
         <div className="postListContainer">
-            <Box sx={{ width: '100%' }}>
+            {/* <Box sx={{ width: '100%' }}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                         <Tab label="Latest" {...a11yProps(0)} />
@@ -56,7 +72,41 @@ const PostList: FC = () => {
                         <Tab label="Best" {...a11yProps(2)} />
                     </Tabs>
                 </Box>
-            </Box>
+            </Box> */}
+
+            <div className="selectBox">
+                {sortType === "time" ?
+                    <div className="latestSelect selected" onClick={latestSelected}>
+                        Latest
+                    </div>
+                    :
+                    <div className="latestSelect" onClick={latestSelected}>
+                        Latest
+                    </div>
+                }
+                {sortType === "like" ?
+                    <div className="mostLikedSelect selected" onClick={mostLikedSelected}>
+                        Most Liked
+
+                    </div>
+                    :
+                    <div className="mostLikedSelect" onClick={mostLikedSelected}>
+                        Most Liked
+
+                    </div>
+                }
+                {sortType === "comment" ?
+                    <div className="mostCommentedSelect selected" onClick={mostCommentedSeleted}>
+                        Most Commented
+
+                    </div>
+                    :
+                    <div className="mostCommentedSelect" onClick={mostCommentedSeleted}>
+                        Most Commented
+
+                    </div>
+                }
+            </div>
             <div className={'postList'}>
                 {status !== 'succeeded' ? <Loader /> : posts.length === 0 ? <div className={'noPosts'}>No posts yet</div> :
                     posts.map(post =>
